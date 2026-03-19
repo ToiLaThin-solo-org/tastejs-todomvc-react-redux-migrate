da5fadceeaf4fa80f246970033656cd64a369fc4
ToiLaThin <ducthingnguyen02@gmail.com>
2026-03-19 14:28:02 +0700

    init commit: setup proj & tooling

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: path.resolve(__dirname, 'src', 'index.tsx'),
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'TodoMVC: React-Redux',
+            template: 'public/index.html',
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+        ],
+    },
+};
