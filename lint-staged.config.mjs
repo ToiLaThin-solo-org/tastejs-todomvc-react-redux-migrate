8f821fb947b8faeb7556252421576f44c6bcaff9
ToiLaThin <ducthingnguyen02@gmail.com>
2026-03-20 22:25:24 +0700

    DEV-19 chore: add github action pipeline config to lint test build

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -4,6 +4,8 @@
  */
 const config = {
     '*.{ts,tsx}': ['npm run lint', 'npm run format'],
-    '**/!(*.ts|*.tsx)': ['npm run format'],
+    '*.{js,json,md,css,scss,html}': ['npm run format'],
+    // 👉 whitelist instead of blacklist, negate not exclude .yaml
+    // '**/!(*.ts|*.tsx)': ['npm run format'],
 };
 export default config;
