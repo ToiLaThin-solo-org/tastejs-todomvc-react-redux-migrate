4e3877011491a7bd739432c50571e73cbfd1841e
ToiLaThin <ducthingnguyen02@gmail.com>
2026-03-23 15:47:23 +0700

    DEV-19 chore: add webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,18 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('node:path');
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        static: './dist',
-        watch: true,
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+            watch: true,
+        },
+        //no refresh when files from static dir changes
         hot: true,
+        liveReload: false,
     },
     module: {
         rules: [
